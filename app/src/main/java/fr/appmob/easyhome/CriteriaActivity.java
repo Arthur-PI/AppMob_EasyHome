8f48cf6bea23b5b2bbca7f144ab628443bdde834
Mohamed772 <mohamed-siso@hotmail.fr>
2021-03-11 12:36:57 +0100

    CriteriaClass, CriteraActivity, AdvertActivity

---
+++ app/src/main/java/fr/appmob/easyhome/CriteriaActivity.java
@@ -0,0 +1,14 @@
+package fr.appmob.easyhome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CriteriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criteria);
+    }
+}
